f3ca4c9819cd517d1c2f5a8e3b60ecb0cfa1a524
muhammadhuwais <uwaisu867@gmail.com>
2024-08-31 15:07:09 +0530

    Update app.py

--- web2/app.py
+++ web2/app.py
@@ -2,8 +2,32 @@ import streamlit as st
 from predict_page import show_predict_page
 from explore_page import show_explore_page
 
-# Set the title of the application
-st.set_page_config(page_title="YouTube Spam Detection", layout="wide")
+
+import streamlit as st
+
+# Set page configuration first
+st.set_page_config(page_title="Streamlit App with Background Image", layout="wide")
+
+# HTML and CSS to set a background image
+background_image = """
+<style>
+.stApp {
+    background-image: url("https://i.postimg.cc/DfxYKD4M/99264900.webp");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+}
+</style>
+"""
+
+# Inject the CSS into the Streamlit app
+st.markdown(background_image, unsafe_allow_html=True)
+
+# Example content to see the effect
+st.title("Welcome to My Streamlit App")
+st.write("This is an example of how to add a background image using HTML and CSS.")
+
 
 # Create a sidebar for navigation
 page = st.sidebar.selectbox("Choose a page", ["Predict", "Explore"])
@@ -13,3 +37,7 @@ if page == "Predict":
     show_predict_page()
 else:
     show_explore_page()
+
+
+
+      
